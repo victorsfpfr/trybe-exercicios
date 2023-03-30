b42fb84eb7776a49a3332bb61ae842b7f706f770
victorsfpfr <victorsfp.fr@gmail.com>
2023-03-30 13:20:44 -0300

    ate exerc 10

--- fundamentos/s3-java-script/dia_02_array-loop-for/dia02.js
+++ fundamentos/s3-java-script/dia_02_array-loop-for/dia02.js
@@ -33,3 +33,83 @@ while (dado1 !== dado2) {
   dado2 = Math.ceil(Math.random() * 6);
   console.log('resultado dado 2:', dado2);
 }
+
+//Exercio 01 A
+{let numbers = [5, 9, 3, 19, 70, 8,100, 2, 35, 27];
+let printarray = numbers;
+console.log(printarray);
+}
+
+//01 B
+{let numbers = [5, 9, 3, 19, 70, 8,100, 2, 35, 27];
+let sum = 0;
+
+for(let index = 0; index < numbers.length; index += 1) {
+    sum = sum + numbers[index];
+}
+console.log(sum); }  
+
+//01 C
+ {
+ let numbers = [5, 9, 3, 19, 70, 8,100, 2, 35, 27];
+ let sum = 0; 
+ let divnumbers = 0; 
+ 
+ for(let index = 0; index < numbers.length; index +=1){
+    sum = sum + numbers[index];
+    divnumbers = sum / numbers.length;
+  } 
+    console.log(divnumbers);
+ }
+
+//01 D
+{
+    let numbers = [5, 9, 3, 19, 70, 8,100, 2, 35, 27];
+    let sum = 0; 
+    let divnumbers = 0; 
+    
+    for(let index = 0; index < numbers.length; index +=1){
+       sum = sum + numbers[index];
+       divnumbers = sum / numbers.length; 
+    } 
+       
+       if (divnumbers >= 20) {
+        console.log("Valor maior que 20")
+       } else {
+         console.log("Valor menor que 20")
+       }      
+       
+    }
+
+   //01 E
+   {
+    let numbers = [5, 9, 3, 19, 70, 8,100, 2, 35, 27];
+    let maiornum = numbers[0];
+    
+    for(let index = 1; index < numbers.length; index += 1){
+      if (numbers[index] > maiornum) {
+        maiornum = numbers[index];
+      } 
+    }
+      console.log(maiornum)
+
+   }
+   //01 F
+   {
+   let numbers = [6, 10, 4, 20, 74, 8,100, 4, 36, 20];
+   let numImpars = 0;
+     for (let index = 0; index < numbers.length; index += 1){
+        if(numbers[index] % 2 !== 0) {
+            numImpars += 1;
+        } 
+    }         
+        if(numImpars === 0) {
+            console.log("Nenhum valor impar encontrado");
+        } else {
+            console.log(numImpars);
+        }
+    }
+
+    //01 G    
+        
+      
