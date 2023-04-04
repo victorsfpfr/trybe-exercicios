adc8ac737502cad407e5eefe646075e9abd84e61
victorsfpfr <victorsfp.fr@gmail.com>
2023-04-04 19:02:36 -0300

    exercio 1

--- fundamentos/s3-java-script/dia_05_let_const_arrow/styleBonus.js
+++ fundamentos/s3-java-script/dia_05_let_const_arrow/styleBonus.js
@@ -1,9 +1,18 @@
 /* 1 – Crie uma função que ligue e desligue o motor de um carro */
 
-let engineOnOff = `on`
 
-let carState = (engineOnOff = `on`) ? `O Carro esta ligado` : `O Carro nao esta ligado`;
-console.log(carState)
+
+const ligarDesligar = (status) => { 
+    status = status === 'desligado' ? 'ligado' : 'desligado';
+    console.log(`O motor esta ${status}`);
+    return status;
+}
+
+ligarDesligar('ligado');
+
+
+
+
 
 /* 2 – Crie uma função que calcule a área de um círculo */
 // A area de um circulo = 2 * PI * raio(raio = circunferencia / 2)
@@ -13,3 +22,5 @@ let circunferenciaCirculo = 200; */
 const calcularAreaCirculo = (circunferencia) => 2 * Math.PI * (circunferencia/2);
 
 console.log(calcularAreaCirculo(200))
+
+/* 3 – Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase */
